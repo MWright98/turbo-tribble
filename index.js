b1df569c080e874ab777b11a4ba7d8f5c43d7671
unknown <mwright7272@gmail.com>
2022-05-01 10:30:09 -0500

    inquirer questions

--- index.js
+++ index.js
@@ -17,7 +17,42 @@ const questions = [
         message: 'What is your email address?'
     },
     {
-
+        type: 'input',
+        name: 'title',
+        message: "What is your project's name?"
+    },
+    {
+        type: 'input',
+        name 'description',
+        message: "Please enter a short description of your project"
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What kind of license does your project have?',
+        choices: ['MIT', 'GPL 3.0', 'BSD 3', 'APACHE 2.0', 'None']
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: 'What command should be run to install program dependencies?',
+        defualt: 'npm i'
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'What command should be entered to run tests?',
+        deafualt: 'npm test'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What information is required to use this repo?'
+    },
+    {
+        type: 'input',
+        name: 'contribute',
+        message: 'What should the user know to contribute to this repo?'
     }
 ];
 
